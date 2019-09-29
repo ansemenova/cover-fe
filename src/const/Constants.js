11d9928d92b04d34688ad0eb3472419df380094e
Anna <asharova@wiley.com>
2019-09-29 11:20:33 +0300

    Refactor

--- src/const/Constants.js
+++ src/const/Constants.js
@@ -1,5 +1,6 @@
 export const INFO_PAGE = '/info';
 export const MAP_PAGE = '/map';
-export const BACKEND_URL = "http://10.70.8.249:8889/mobiletrack?operator=";
+/*export const BACKEND_URL = "http://10.70.8.249:8889/mobiletrack?operator=";*/
+export const BACKEND_URL = "http://localhost:8889/mobiletrack?operator=";
 export const GEO_CODER_URL = "https://nominatim.openstreetmap.org/search/";
 export const GEO_CODER_URL_PART = "?format=json&addressdetails=1&limit=1&polygon_svg=1";
